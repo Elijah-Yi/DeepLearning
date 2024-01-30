50a426c489e253debd794a6237d2079f92864851
Elijah-Yi <540497358@qq.com>
2024-01-30 09:40:04 +0800

    update script

--- pytorch_classification/Dataset/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/python3.9
-# -*- coding:utf-8 -*-
-"""
-@Author   : Haiy. Yi
-@Time     : 2024/1/19 2:08 PM
-@File     : __init__.py
-@Software : PyCharm
-@System   : MacOS catalina
-"""
--- pytorch_classification/Script/lenet.sh
+++
@@ -1,3 +0,0 @@
-
-torchrun --nproc_per_node=4 --nnodes 1 --node_rank 0 --master_addr='192.168.210.5' --master_port='9999' Tools/train.py \
-  --cfg lenet.yaml
